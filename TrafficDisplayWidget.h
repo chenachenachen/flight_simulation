7e4ff6b1cde9afbec0fbb0fa4879539a08def7aa
chenachenachen <57134872+chenachenachen@users.noreply.github.com>
2026-03-26 16:32:13 +0100

    Update TrafficDisplayWidget.h

--- TrafficDisplayWidget.h
+++ TrafficDisplayWidget.h
@@ -25,18 +25,6 @@ private slots:
 private:
     AircraftManager *m_manager;
     QTimer *m_refreshTimer;
-    
-    // 绘制函数
-    void drawBackground(QPainter &painter);
-    void drawAircrafts(QPainter &painter);  // 绘制BlueSky交通飞机（红色方框）
-    // drawOwnship 已移除 - 本机由X-Plane界面显示
-    void drawInfo(QPainter &painter);
-    
-    // 坐标转换
-    QPointF worldToScreen(double lat, double lon);
-    
-    // 缩放因子
-    double m_scale;
 };
 
 #endif
